2a878328fb1fcb226f80bd6d7daada254bd5dbdd
Gorodeckii912 <d.gavrilenko912@gmail.com>
2023-05-11 17:56:50 +0100

    999

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,10 +1,7 @@
 import './js/modal';
 import './js/toggel';
-
-
-// при клике на кнопку переход на каталог 
-const searchMovieBtn = document.getElementById("search-movie-btn");
-
-searchMovieBtn.addEventListener("click", function() {
-  window.location.replace("catalog.html");
-});
+import './js/localStorage'
+import './js/checkLocalStorage'
+import './js/localModalJs';
+import './js/modals-open-close';
+import './js/scrollUp';
